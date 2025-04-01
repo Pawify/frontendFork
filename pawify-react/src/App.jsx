92f28b8951315979613335e904bba61eb7ac9504
KevinDaniel18 <kevnsc18@gmail.com>
2025-04-01 16:17:54 -0500

    first register version

--- pawify-react/src/App.jsx
+++ pawify-react/src/App.jsx
@@ -1,13 +1,9 @@
-//Ejemplo de como usar las variable de colores
-import theme from "./constants/themes"; //importacion
+import Register from "./app/components/registerUser/Register";
 
 function App() {
-  const { softBeige } = theme.colors; //desestructuracion
-
   return (
     <div>
-      {/* implementacion */}
-      <p style={{ color: softBeige }}>Changes</p> 
+      <Register />
     </div>
   );
 }
