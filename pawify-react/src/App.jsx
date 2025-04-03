1d52edc1edddb0fdaf007c0d0612c8b49424ef6b
PabloRM94 <pablorodriguezmadurga@gmail.com>
2025-04-03 16:21:20 +0200

    añadido para usar el react-router-dom

--- pawify-react/src/App.jsx
+++ pawify-react/src/App.jsx
@@ -1,10 +1,18 @@
-import Register from "./app/components/registerUser/Register";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './app/components/login/login';
+import Register from './app/components/registerUser/Register';
 
 function App() {
   return (
-    <div>
-      <Register />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* Agrega otras rutas según sea necesario */}
+        <Route path="/" element={<Login />} /> {/* Ruta por defecto */}
+      </Routes>
+    </Router>
   );
 }
 
