bc11a2e0f54e0b551b38571c7299504bfc529f1f
PabloRM94 <pablorodriguezmadurga@gmail.com>
2025-04-03 16:19:25 +0200

    conexion con la api

--- pawify-react/vite.config.js
+++ pawify-react/vite.config.js
@@ -6,6 +6,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://4400-90-74-219-163.ngrok-free.app/users',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
