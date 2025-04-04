8bd0cffd0fe640286fa512eaeccad82d1b926974
PabloRM94 <pablorodriguezmadurga@gmail.com>
2025-04-04 11:24:54 +0200

    Añadidos archivos de página 404 desde pawify-dev-front

---
+++ pawify-react/src/app/errors/NotFound.jsx
@@ -0,0 +1,34 @@
+import theme from '@/constants/themes';
+import catdog404 from '/catdog404.png'
+
+export  default function  NotFound () {
+
+  const { accent } = theme.colors;
+
+    return (
+      <section className="max-w-md mx-auto my-44 px-6 text-lg">
+
+        <div className="flex justify-center">
+          <img src={catdog404} alt="cat-dog-404" className="max-w-[80%] relative top-14 sm:top-10" />
+        </div>
+        <div className="flex flex-col items-center text-center justify-evenly p-20 bg-gray-100 rounded-2xl shadow-[5px_20px_20px_10px_rgba(0,0,0,0.25)]">
+          <h1 className="text-6xl text-gray-500 font-bold">404</h1>
+          <h2 className="text-2xl font-bold mt-3 mb-3">Page not found</h2>
+          <p className="text-gray-600">
+            The page you are looking for no longer exists. Perhaps you can return
+            back to the site's homepage and see if you can find what you are
+            looking for.
+          </p>
+          <div className="flex items-center gap-8 mt-5">
+            <a
+              style={{ backgroundColor: accent}}
+              href="/"
+              className="text-black py-2 px-5 mt-5 rounded-2xl font-semibold transition-opacity hover:opacity-90"
+            >
+              Back home
+            </a>
+          </div>
+        </div>
+      </section>
+    );
+  };
