e2ea89633a926ec6d73ab7c1e16aa7e3be8f9b70
alberto <albertoguerra4@gmail.com>
2025-04-04 19:29:28 +0200

    Cambios visuales, responsive, enlace a Home, enlace a Perfil para registro

--- pawify-react/src/app/components/navBar/navBar.jsx
+++ pawify-react/src/app/components/navBar/navBar.jsx
@@ -2,13 +2,23 @@
 import React from "react";
 import "./navBar.css";
 import theme from "@/constants/themes";
+import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import { profileSvg, searchSvg } from "./svg";
 
 const NavBar = () => {
+  const navigate = useNavigate(); // hook para redirigir
+
+  const handleProfileClick = () => {
+    navigate('login'); // Aquí pones la ruta a la que quieres redirigir
+  };
+
   return (
     <nav id="nav" style={{ backgroundColor: theme.colors.secondary }}>
       <div className="section-one">
-        <img src="/LogoPaw.png" alt="Logo PawPaw" />
+        <Link to="/" id="logo-link">
+          <img src="/LogoPaw.png" alt="Logo PawPaw" className="logo" />
+        </Link>
       </div>
       <div className="section-two">
         <form className="form">
@@ -21,30 +31,19 @@ const NavBar = () => {
           </button>
         </form>
         <ul className="nav-links">
-          <li id="li">
-            <a href="/">Home</a>
-          </li>
-          <li id="li">
-            <a href="/adopciones">Adopciones</a>
-          </li>
-          <li id="li">
-            <a href="/voluntarios">Voluntarios</a>
-          </li>
-          <li id="li">
-            <a href="/blog">Blog</a>
-          </li>
-          <li id="li">
-            <a href="/servicios">Servicios</a>
-          </li>
-          <li id="li">
-            <a href="/tienda">Tienda</a>
-          </li>
+          <li id="li"><a href="/">Home</a></li>
+          <li id="li"><a href="/adopciones">Adopciones</a></li>
+          <li id="li"><a href="/voluntarios">Voluntarios</a></li>
+          <li id="li"><a href="/blog">Blog</a></li>
+          <li id="li"><a href="/servicios">Servicios</a></li>
+          <li id="li"><a href="/tienda">Tienda</a></li>
         </ul>
       </div>
       <div className="section-three">
         <button
           id="profile-btn"
           style={{ backgroundColor: theme.colors.accent }}
+          onClick={handleProfileClick}
         >
           {profileSvg}
         </button>
