40ce42bcc4f9c82f795508bc5635b5a54dac5a8d
PabloRM94 <pablorodriguezmadurga@gmail.com>
2025-04-04 10:43:31 +0200

    Revert "Merge branch 'pawify-dev-front' into PabloRM"
    This reverts commit 7d1975d037c6735f2c2b8b3edf0d0fe52f13778f, reversing
    changes made to 8b058f51584b2e54e0349b1901a8fe0be8a968b1.

--- pawify-react/src/App.jsx
+++ pawify-react/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./app/components/registerUser/Register";
 import Login from "./app/components/login/Login";
 import Home from "./app/components/pages/Home";
-import  NotFound  from "./app/errors/NotFound";
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
