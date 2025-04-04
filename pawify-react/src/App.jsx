e57c32fe25d49f5a37b8c3fb484eca5e8807fcb0
PabloRM94 <pablorodriguezmadurga@gmail.com>
2025-04-04 11:28:44 +0200

    add notfound

--- pawify-react/src/App.jsx
+++ pawify-react/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./app/components/registerUser/Register";
 import Login from "./app/components/login/Login";
 import Home from "./app/components/pages/Home";
+import  NotFound  from "./app/errors/NotFound";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
